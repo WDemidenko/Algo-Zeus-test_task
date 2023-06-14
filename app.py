14ca2c183a3d9c32e3aa6ca37e15032d8890a7e0
Vladyslav Demydenko <bublikyo3@gmail.com>
2023-06-14 23:52:00 +0200

    Fix code with black

--- app.py
+++ app.py
@@ -1,47 +1,70 @@
 from flask import Flask, render_template, request
+from main import get_candlestick_data, save_to_csv, get_market_caps
 
 import plotly.express as px
 import pandas as pd
 import plotly.graph_objects as go
 
-from main import get_candlestick_data, save_to_csv, get_market_caps
 
 app = Flask(__name__)
 
-symbols = ["ADAUSDT", "DOGEUSDT", "TRXUSDT", "MATICUSDT", "SOLUSDT", "LTCUSDT", "DOTUSDT", "BUSDDAI", "AVAXUSDT",
-           "SHIBUSDT"]
+SYMBOLS = [
+    "ADAUSDT",
+    "DOGEUSDT",
+    "TRXUSDT",
+    "MATICUSDT",
+    "SOLUSDT",
+    "LTCUSDT",
+    "DOTUSDT",
+    "BUSDDAI",
+    "AVAXUSDT",
+    "SHIBUSDT",
+]
 
 
-@app.route('/candlestick')
+@app.route("/candlestick")
 def candlestick():
-    symbol = request.args.get('symbol', default='BTCUSDT')
-    interval = request.args.get('interval', default='1m')
+    symbol = request.args.get("symbol", default="BTCUSDT")
+    interval = request.args.get("interval", default="1m")
 
     candlestick_data = get_candlestick_data(symbol, interval)
     save_to_csv(candlestick_data, symbol, interval)
 
-    df = pd.read_csv(f'{symbol}-{interval}.csv')
+    df = pd.read_csv(f"{symbol}-{interval}.csv")
 
-    candlestick_df = go.Figure(data=[go.Candlestick(x=df['Open time'],
-                                                 open=df['Open'],
-                                                 high=df['High'],
-                                                 low=df['Low'],
-                                                 close=df['Close'])])
+    candlestick_df = go.Figure(
+        data=[
+            go.Candlestick(
+                x=df["Open time"],
+                open=df["Open"],
+                high=df["High"],
+                low=df["Low"],
+                close=df["Close"],
+            )
+        ]
+    )
     candlestick_json = candlestick_df.to_json()
 
-    return render_template('candlestick.html', candlestick=candlestick_json, symbol=symbol, interval=interval)
+    return render_template(
+        "candlestick.html",
+        candlestick=candlestick_json,
+        symbol=symbol,
+        interval=interval,
+    )
 
 
-@app.route('/piechart')
+@app.route("/piechart")
 def piechart():
-    market_caps = get_market_caps(symbols)
-    df_market_caps = pd.DataFrame({'Symbol': symbols, 'Market Cap': market_caps})
+    market_caps = get_market_caps(SYMBOLS)
+    df_market_caps = pd.DataFrame(
+        {"Symbol": SYMBOLS, "Market Cap": market_caps}
+    )
 
-    pie = px.pie(df_market_caps, values='Market Cap', names='Symbol')
+    pie = px.pie(df_market_caps, values="Market Cap", names="Symbol")
     pie_json = pie.to_json()
 
-    return render_template('piechart.html', pie=pie_json)
+    return render_template("piechart.html", pie=pie_json)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from datetime import datetime
+from flask import abort
 
 import requests
 import csv
-
-from flask import abort
 
 
 def get_candlestick_data(symbol, interval):
@@ -18,33 +17,33 @@ def get_candlestick_data(symbol, interval):
 
 
 def save_to_csv(data, symbol, interval):
-
     filename = f"{symbol}-{interval}.csv"
     with open(filename, "w", newline="") as file:
         writer = csv.writer(file)
-        writer.writerow([
-            "Open time",
-            "Open",
-            "High",
-            "Low",
-            "Close",
-            "Volume",
-            "Close time",
-            "Quote asset volume",
-            "Number of trades",
-            "Taker buy base asset volume",
-            "Taker buy quote asset volume"
-        ])
+        writer.writerow(
+            [
+                "Open time",
+                "Open",
+                "High",
+                "Low",
+                "Close",
+                "Volume",
+                "Close time",
+                "Quote asset volume",
+                "Number of trades",
+                "Taker buy base asset volume",
+                "Taker buy quote asset volume",
+            ]
+        )
         for row in data:
             open_time = datetime.fromtimestamp(int(row[0]) / 1000)
             close_time = datetime.fromtimestamp(int(row[6]) / 1000)
-            row[0] = open_time.strftime('%Y-%m-%d %H:%M:%S')
-            row[6] = close_time.strftime('%Y-%m-%d %H:%M:%S')
+            row[0] = open_time.strftime("%Y-%m-%d %H:%M:%S")
+            row[6] = close_time.strftime("%Y-%m-%d %H:%M:%S")
             writer.writerow(row[:11])
 
 
 def get_market_caps(symbols):
-
     url = "https://www.binance.com/exchange-api/v2/public/asset-service/product/get-products"
     response = requests.get(url)
     data = response.json()
